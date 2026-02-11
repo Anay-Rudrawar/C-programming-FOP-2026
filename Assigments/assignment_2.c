7f95747525078c0c85f0b4c20458dff1cab0feb6
Anay-Rudrawar <anayrudrawar44@gmail.com>
2026-02-11 18:19:49 +0000

    Implement multiplication case in basic calculator in assignment_2.c

--- Assigments/assignment_2.c
+++ Assigments/assignment_2.c
@@ -30,5 +30,10 @@ int main() {
                 printf("Result: %d - %d = %d\n", num1, num2, result);
                 break;
 
-           
+            case 3:
+                result = num1 * num2;
+                printf("Result: %d * %d = %d\n", num1, num2, result);
+                break;
+
+            
 }
