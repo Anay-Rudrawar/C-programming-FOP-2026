693b8f0e28de94bafd68140e1f7227fa3624fb1d
Anay-Rudrawar <anayrudrawar44@gmail.com>
2026-02-11 18:15:30 +0000

    Fix formatting and structure of leap year output in assignment_1.c

--- Assigments/assignment_1.c
+++ Assigments/assignment_1.c
@@ -8,5 +8,9 @@ int main() {
 
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         printf("%d is a Leap Year.\n", year);
-   
+    } else {
+        printf("%d is NOT a Leap Year.\n", year);
+    }
+
+    return 0;
 }
