98a35caeeaa7c589affcd07e3e85cc0c1f5554da
Anay-Rudrawar <anayrudrawar44@gmail.com>
2026-02-11 18:25:26 +0000

    Fix formatting and add multiplication table logic in assignment_3.c

--- Assigments/assignment_3.c
+++ Assigments/assignment_3.c
@@ -8,5 +8,9 @@ int main() {
 
     printf("\nMultiplication Table of %d:\n", num);
 
-    
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
 }
