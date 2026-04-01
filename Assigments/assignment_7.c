668de17d79ba5e12af70151bd611bf30106d42fe
Anay-Rudrawar <anayrudrawar44@gmail.com>
2026-04-01 18:01:32 +0000

    Add program to accept student details and display results using an array of structures in assignment_7.c

---
+++ Assigments/assignment_7.c
@@ -0,0 +1,35 @@
+// accept student display result using an array of structure
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll_number;
+    float marks;
+};
+
+int main() {
+    int n;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct Student students[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_number);
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Name: %s, Roll Number: %d, Marks: %.2f\n", students[i].name, students[i].roll_number, students[i].marks);
+    }
+
+    return 0;
+}
